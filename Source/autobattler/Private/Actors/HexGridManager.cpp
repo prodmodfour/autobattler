ad4ff6dd7cd5b4e8fa521b9ad61126a2e5af694a
prodmodfour <prodmodfour@gmail.com>
2025-05-27 18:22:31 +0100

    Implemented core functionality for AHexGridManager, including grid initialization, tile data management, unit placement, and tile validation methods.

---
+++ Source/autobattler/Private/Actors/HexGridManager.cpp
@@ -0,0 +1,73 @@
+#include "Actors/HexGridManager.h"
+
+AHexGridManager::AHexGridManager()
+{
+    PrimaryActorTick.bCanEverTick = false;
+    /* This is the same size as other autochess style games like dota autochess and TFT */
+    GridWidth = 7;  
+    GridHeight = 8; 
+
+}
+
+void AHexGridManager::BeginPlay()
+{
+    Super::BeginPlay();
+    InitializeGrid(GridWidth, GridHeight); 
+}
+
+void AHexGridManager::InitializeGrid(int32 Width, int32 Height)
+{
+    GridWidth = Width;
+    GridHeight = Height;
+    GridTiles.Empty();
+    GridTiles.SetNum(GridWidth * GridHeight); 
+
+    for (int32 y = 0; y < GridHeight; ++y)
+    {
+        for (int32 x = 0; x < GridWidth; ++x)
+        {
+            FIntPoint Coords(x, y);
+            GridTiles[GetIndexForCoordinates(Coords)] = FHexTileData(Coords);
+        }
+    }
+}
+
+FHexTileData* AHexGridManager::GetTileData(const FIntPoint& GridCoordinates)
+{
+    if (!IsTileValid(GridCoordinates))
+    {
+        return nullptr;
+    }
+    return &GridTiles[GetIndexForCoordinates(GridCoordinates)];
+}
+
+bool AHexGridManager::SetUnitOnTile(AUnitCharacter* UnitToPlace, const FIntPoint& GridCoordinates)
+{
+    FHexTileData* Tile = GetTileData(GridCoordinates);
+    if (Tile /* && Tile->OccupyingUnit == nullptr */) // Add occupation check later if needed by tests
+    {
+        Tile->OccupyingUnit = UnitToPlace;
+        return true;
+    }
+    return false;
+}
+
+AUnitCharacter* AHexGridManager::GetUnitAtCoordinates(const FIntPoint& GridCoordinates) const
+{
+    if (!IsTileValid(GridCoordinates))
+    {
+        return nullptr;
+    }
+    return GridTiles[GetIndexForCoordinates(GridCoordinates)].OccupyingUnit.Get();
+}
+
+bool AHexGridManager::IsTileValid(const FIntPoint& GridCoordinates) const
+{
+    return GridCoordinates.X >= 0 && GridCoordinates.X < GridWidth &&
+           GridCoordinates.Y >= 0 && GridCoordinates.Y < GridHeight;
+}
+
+int32 AHexGridManager::GetIndexForCoordinates(const FIntPoint& GridCoordinates) const
+{
+    return GridCoordinates.Y * GridWidth + GridCoordinates.X;
+}

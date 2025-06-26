8fd3d461b610b4afd32287e37114c60a2afda8a9
Ashraf <prodmodfour@gmail.com>
2025-06-26 01:46:44 +0100

    Added logic for WBP_ShopItem

---
+++ Source/autobattler/Private/UI/ShopItemWidget.cpp
@@ -0,0 +1,21 @@
+#include "UI/ShopItemWidget.h" 
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UShopItemWidget::Initialize(const FUnitData& UnitData)
+{
+    if (Text_UnitName)
+    {
+        Text_UnitName->SetText(UnitData.DisplayName);
+    }
+
+    if (Text_Cost)
+    {
+        Text_Cost->SetText(FText::AsNumber(UnitData.Cost));
+    }
+
+    if (Image_Portrait)
+    {
+        Image_Portrait->SetBrushFromTexture(UnitData.PortraitTexture);
+    }
+}
---
+++ Source/autobattler/Public/UI/ShopItemWidget.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Data/UnitDataTypes.h" /
+#include "ShopItemWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+class UButton;
+
+UCLASS()
+class AUTOBATTLER_API UShopItemWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+
+    UFUNCTION(BlueprintCallable, Category = "Shop Item")
+    void Initialize(const FUnitData& UnitData);
+
+protected:
+
+    UPROPERTY(meta = (BindWidget))
+    UImage* Image_Portrait;
+
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* Text_UnitName;
+
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* Text_Cost;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* Button_Buy;
+};

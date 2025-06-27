e63310b51b235611b970dc192c29538631ba3703
Ashraf <prodmodfour@gmail.com>
2025-06-27 14:41:53 +0100

    Added Documentation to include statement

--- Source/autobattler/Public/autobattler.h
+++ Source/autobattler/Public/autobattler.h
@@ -20,13 +20,13 @@
  * This section contains all the data tables used in the game, automatically generated from the project's UDataTables.
  *
  * @section DT_Items Items Data Table
- * \include generated/DataTables/DT_Items.md
+ * \include Documentation/generated/DataTables/DT_Items.md
  *
  * @section DT_Units Units Data Table
- * \include generated/DataTables/DT_Units.md
+ * \include Documentation/generated/DataTables/DT_Units.md
  *
  * @section DT_Traits Traits Data Table
- * \include generated/DataTables/DT_Traits.md
+ * \include Documentation/generated/DataTables/DT_Traits.md
  *
  */
 

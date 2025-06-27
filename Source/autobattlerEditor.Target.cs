da4037527e03e97464545e5fed13e30477717b35
Ashraf <prodmodfour@gmail.com>
2025-06-27 02:48:53 +0100

    Build now removed old doxygen generated files before making new ones

--- Source/autobattlerEditor.Target.cs
+++ Source/autobattlerEditor.Target.cs
@@ -12,7 +12,11 @@ public class autobattlerEditorTarget : TargetRules
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("autobattler");
 
-   		string command = "cd \"$(ProjectDir)\" && doxygen Doxyfile";
-        PostBuildSteps.Add($"echo \"Running Doxygen...\" && {command}");
+
+        string command = "cd \"$(ProjectDir)\" && " +
+                         "if exist \"Documentation\\generated\" ( rd /s /q \"Documentation\\generated\" ) && " +
+                         "doxygen Doxyfile";
+
+        PostBuildSteps.Add($"echo \"Cleaning and running Doxygen...\" && {command}");
 	}
 }

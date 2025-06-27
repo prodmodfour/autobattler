0b05b6939d33d77517094e18cf2de0f57bd2d8b3
Ashraf <prodmodfour@gmail.com>
2025-06-27 13:51:03 +0100

    Added doxygen comments in order to get the md tables into the documentation. This currently isn't working

---
+++ Source/autobattler/Documentation.h
@@ -0,0 +1,16 @@
+/**
+ * @page DataTables Game Data Tables
+ *
+ * This section contains all the data tables used in the game, automatically generated from the project's UDataTables.
+ *
+ * @section DT_Items Items Data Table
+ * \include generated/DataTables/DT_Items.md
+ *
+ * @section DT_Units Units Data Table
+ * \include generated/DataTables/DT_Units.md
+ * 
+ * @section DT_Traits Traits Data Table
+ * \include generated/DataTables/DT_Traits.md
+ * 
+ * 
+ */

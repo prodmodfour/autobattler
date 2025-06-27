caa49733d7d58dee489e1682170f4eb6196afea0
Ashraf <prodmodfour@gmail.com>
2025-06-27 03:23:45 +0100

    Build now makes Documentation/generated if it doesn't already exist

--- Source/autobattlerEditor.Target.cs
+++ Source/autobattlerEditor.Target.cs
@@ -12,9 +12,14 @@ public class autobattlerEditorTarget : TargetRules
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("autobattler");
 
-
+        // This command chain will:
+        // 1. Change to the project's root directory.
+        // 2. If the 'generated' folder exists, quietly remove it and all its contents.
+        // 3. Create a new, empty 'generated' folder.
+        // 4. Run Doxygen.
         string command = "cd \"$(ProjectDir)\" && " +
                          "if exist \"Documentation\\generated\" ( rd /s /q \"Documentation\\generated\" ) && " +
+                         "mkdir \"Documentation\\generated\" && " +
                          "doxygen Doxyfile";
 
         PostBuildSteps.Add($"echo \"Cleaning and running Doxygen...\" && {command}");

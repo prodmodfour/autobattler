9fe18b59af24bddf54b7e1cd9c5b7c0c238ee26b
Ashraf <prodmodfour@gmail.com>
2025-06-28 12:34:57 +0100

    Created a basic combat system test

---
+++ Source/autobattlerTests/Private/Test_CombatSystem.cpp
@@ -0,0 +1,33 @@
+#include "CoreMinimal.h"
+#include "Misc/AutomationTest.h"
+#include "Tests/AutomationEditorCommon.h"
+#include "Engine/World.h"
+#include "AUnitCharacter.h" 
+#include "Engine/DataTable.h"
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FCombatSystemTest, "Autobattler.Tests.Combat.UnitInitialization", 
+                                EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter)
+
+bool FCombatSystemTest::RunTest(const FString& Parameters)
+{
+
+    UWorld* World = FAutomationEditorCommonUtils::CreateNewMap();
+
+
+    UDataTable* UnitDataTable = LoadObject<UDataTable>(nullptr, TEXT("/Game/Autobattler/Data/DT_Units.DT_Units"));
+    TestNotNull(TEXT("Unit DataTable should be loaded"), UnitDataTable);
+
+    AUnitCharacter* TestUnit = World->SpawnActor<AUnitCharacter>();
+    TestNotNull(TEXT("TestUnit should be successfully spawned"), TestUnit);
+
+    TestUnit->UnitDataTable = UnitDataTable;
+    TestUnit->UnitID = FName("TestWarrior");
+
+    TestUnit->DispatchBeginPlay();
+
+    FUnitData* TestWarriorData = UnitDataTable->FindRow<FUnitData>(FName("TestWarrior"), "");
+    TestEqual(TEXT("Unit's health should be initialized from DataTable"), TestUnit->CurrentHealth, TestWarriorData->BaseHealth);
+
+    return true;
+}
